01f13ee88acc92653388d6f19a01d54660cc5079
Dimas Tri Aditya <85581902+dtriadtya@users.noreply.github.com>
2022-04-05 21:13:39 +0700

    Add files via upload

---
+++ Tugas Akhir Pertemuan 6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+ 
+main() {
+   bool ketemu;
+   int posisi[25];
+   int c,i,banyak=0;
+   int data[25]={34,8,50,74,87,90,12,25,20,30,35,45,40,22,29,72,60,55,53,12,32,33,12,41,12};
+   cout<<"Data : ";
+  
+  for(i=0;i<25;i++){
+  cout<<data[i]<<" ";
+ }
+  
+   cout<<"\n\nData yang dicari : ";
+     cin>>c;
+     for (i=0;i<25;i++) {
+     if (data[i]==c) {
+     ketemu=true;
+     posisi[banyak]=i+1;
+     banyak++;
+  }
+    }
+  
+    if (ketemu){
+    cout<<"Data : "<<c;
+    cout<<"\nDitemukan sebanyak "<<banyak;
+    cout<<"\nPada posisi ke ";
+   
+   for(i=0;i<banyak;i++){
+   cout<<posisi[i]<<" ";
+  }
+    }
+ else
+ {
+  cout<<"Data "<<c<<" tidak ditemukan";
+ }
+ getch();
+}
